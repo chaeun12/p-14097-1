bc0e94ae21a9fdc4755beada120d78b81a12cc21
chaeun12 <145729167+chaeun12@users.noreply.github.com>
2025-11-21 15:37:30 +0900

    p-14097-1

---
+++ src/main/java/com/back/AppConfig.java
@@ -0,0 +1,10 @@
+package com.back;
+
+import org.springframework.context.annotation.Configuration;
+import org.springframework.stereotype.Component;
+
+@Configuration
+@Component
+public class AppConfig {
+
+}
