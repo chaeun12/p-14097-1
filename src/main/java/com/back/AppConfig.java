2f53dbddc04c77452347782fca765ba8b61875cd
chaeun12 <145729167+chaeun12@users.noreply.github.com>
2025-11-21 15:44:25 +0900

    p-14097-1

--- src/main/java/com/back/AppConfig.java
+++ src/main/java/com/back/AppConfig.java
@@ -1,10 +1,20 @@
 package com.back;
 
+import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.stereotype.Component;
 
 @Configuration
 @Component
 public class AppConfig {
+    @Bean
+    PersonRepository personRepository() {
+        return new PersonRepository(1);
+    }
+
+    @Bean
+    PersonRepository personRepositoryV2() {
+        return new PersonRepository(2);
+    }
 
 }
